77501b7af6eedac07a68c33564bff5386713216c
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2025-05-30 17:00:18 +0800

    To fix: check auth

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
+import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config()
@@ -8,6 +9,8 @@ const app = express();
 
 const PORT = process.env.PORT
 
+app.use(express.json());
+app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
